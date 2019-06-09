024c10c71641b9894c87c21759b69257f24c5ae6
github username <github email address>
2019-06-09 15:45:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function repeat(str, num) {
+	return (new Array(num + 1)).join(str)
+}
+
+function heyJude() {
+	return repeat("Na na na na na na na, na na na na, hey Jude.", 16)
+}
+
+function prohibitedLanguage(str) {
+	let banned = /candycorn|brusselssprouts/
+	return str.search(banned)
+}
