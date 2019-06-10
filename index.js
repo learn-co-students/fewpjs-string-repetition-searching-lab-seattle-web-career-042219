0bebd913da99ce14451614d08ff625c915a9f469
github username <gregm9999@gmail.com>
2019-06-09 22:04:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+function heyJude() {
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+}
+
+function prohibitedLanguage(str) {
+  const myWords = /candycorn|brusselssprouts/;
+  return str.search(myWords);
+}
