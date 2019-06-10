0f22fb2f63f1f5fa8a644642fed209705893e256
github username <gregm9999@gmail.com>
2019-06-09 22:05:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here
 
-function heyJude() {
-  return "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+function heyJude(na) {
+  return na.repeat(16);
 }
 
 function prohibitedLanguage(str) {
