1d8c5b81ec7a17c56824a5885e31ee0cf0d67989
austenhasty <hastyae@gmail.com>
2019-06-11 15:36:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude(n) {
+  return n.repeat(16)
+}
+
+function prohibitedLanguage(w) {
+  let bad_words  = /candycorn|brusselssprouts/
+  return w.search(bad_words)
+}
