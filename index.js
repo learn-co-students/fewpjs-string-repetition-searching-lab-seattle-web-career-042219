10291c9192dc91925ad54132019b71d60ee440d4
Danny Tseng <github email address>
2019-06-12 13:16:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function repeat(str, num) {
+  return (new Array(num+1)).join(str);
+}
+
+function heyJude(str) {
+  return repeat(str, 16);
+}
+
+function prohibitedLanguage(string) {
+  let target = /candycorn|brusselssprouts/;
+  return string.search(target);
+}
