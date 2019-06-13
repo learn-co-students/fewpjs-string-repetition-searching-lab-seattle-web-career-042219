3463fdb26928946b75e735cb6b4ddb0db6be19da
github username <github email address>
2019-06-12 20:36:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+function heyJude(){
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+function prohibitedLanguage(str){
+    return str.search(/pineapplepizza|candycorn|brusselssprouts/)
+}
